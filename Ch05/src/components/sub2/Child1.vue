b1d2c7d9544c0a4208bba7afdc85bedfafda1713
mh990407 <dlwlzjffj1@naver.com>
2023-04-11 10:34:36 +0900

    2023/04/10 router 실습

---
+++ Ch05/src/components/sub2/Child1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>Child1</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: "Child1",
+  setUp() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+div {
+  border: 1px solid green;
+}
+</style>
