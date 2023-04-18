00216d73dc53b77bce72cc90859f2443d59e7a63
mh990407 <dlwlzjffj1@naver.com>
2023-04-18 13:48:05 +0900

    2023/04/18  Grids 실습

--- Ch08/src/components/sub2/Grids.vue
+++ Ch08/src/components/sub2/Grids.vue
@@ -45,6 +45,16 @@
       <v-col md="4" lg="12" class="border text-center">sm3</v-col>
     </v-row>
   </v-container>
+
+  <h4>반응형 레이아웃</h4>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column1</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column2</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column3</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column4</v-col>
+    </v-row>
+  </v-container>
 </template>
 <script setup></script>
 <style scoped></style>
