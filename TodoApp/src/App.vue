d655be72f73a888ebd209908de8a05acb7c5cee6
mh990407 <dlwlzjffj1@naver.com>
2023-04-20 12:06:06 +0900

    Todo App 실습

---
+++ TodoApp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>TodoApp</h3>
+  <Header v-on:addTodo="addTodoHandler"></Header>
+  <Main></Main>
+  <Footer></Footer>
+</template>
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+import { useStore } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  setup() {
+    const store = useStore();
+
+    const addTodoHandler = (value) => {
+      alert(value);
+      store.dispatch("addTodo", value);
+    };
+
+    return { addTodoHandler };
+  },
+};
+</script>
+<style scoped></style>
---
+++ TodoApp/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>Footer</h3>
+</template>
+<script>
+export default {
+  name: "Footer",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+footer {
+  width: 100px;
+  height: auto;
+  text-align: center;
+}
+footer > button {
+  width: 100px;
+  height: 30px;
+  color: #e20303;
+}
+</style>
